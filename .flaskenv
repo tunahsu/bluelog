f2f9d8d2e7829edf3ece1062d2f659e51f51b5a6
Tuna Hsu <tunahsu45@gmail.com>
2018-11-20 15:42:35 +0800

    Set up basic configuration and define models

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=blueblog
+FLASK_CONFIG=development
---
+++ bluelog/extensions.py
@@ -0,0 +1,11 @@
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+from flask_ckeditor import CKEditor
+from flask_moment import Moment
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
+ckeditor = CKEditor()
+moment = Moment()
